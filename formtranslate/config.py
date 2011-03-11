28cd5f3070d6abdc659d393a138ec2d98c814285
Daniel Roberts <droberts@dimagi.com>
2011-03-11 11:28:33 -0500

    remove a print statement

--- formtranslate/config.py
+++ formtranslate/config.py
@@ -6,4 +6,3 @@ _DEFAULT_FORM_TRANSLATE_JAR_LOCATION = os.path.join(os.path.abspath(os.path.dirn
 
 FORM_TRANSLATE_JAR_LOCATION = getattr(settings, "FORM_TRANSLATE_JAR_LOCATION", 
                                       _DEFAULT_FORM_TRANSLATE_JAR_LOCATION)
-print FORM_TRANSLATE_JAR_LOCATION 
