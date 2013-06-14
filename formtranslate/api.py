88bc07a44df9bef774533d06eba9b8b57d9745ee
yedi <yedispaghetti@gmail.com>
2013-06-14 14:43:51 -0400

    use new formtranslate jar

--- formtranslate/api.py
+++ formtranslate/api.py
@@ -4,13 +4,12 @@ from django.conf import settings
 from formtranslate import config
 from tempfile import NamedTemporaryFile
 import os
+import json
 
 def validate(input_data, version='1.0'):
     """Validates an xform into an xsd file"""
     # hack
-    vals = form_translate(input_data, "schema", version=version)
-    vals["outstring"] = ""
-    return vals
+    return form_translate(input_data, "validate", version=version)
 
 def get_xsd_schema(input_data, version='1.0'):
     """Translates an xform into an xsd file"""
@@ -51,33 +50,25 @@ def form_translate(input_data, operation, version='1.0'):
     #
     # You can pass in a filename or a full string/stream of xml data
 
-    stdout_w = stderr_w = stdin_w = None
+    stdout_w = stdin_w = None
     location = config.get_form_translate_jar_location(version)
 
     try:
         with open_w() as stdin_w:
             stdin_w.write(input_data)
 
-        with nested(open_w(), open_w(), open_r(stdin_w)) as (stdout_w, stderr_w, stdin_r):
+        with nested(open_w(), open_r(stdin_w)) as (stdout_w, stdin_r):
             Subprocess(
                 ["java","-jar", location, operation],
                 shell=False,
                 stdin=stdin_r,
                 stdout=stdout_w,
-                stderr=stderr_w
             ).run(timeout=getattr(settings, 'FORMTRANSLATE_TIMEOUT', 5))
 
         with open_r(stdout_w) as stdout_r:
             output = stdout_r.read()
-        with open_r(stderr_w) as stderr_r:
-            error = stderr_r.read()
     finally:
-        for file in (stdout_w, stderr_w, stdin_w):
+        for file in (stdout_w, stdin_w):
             delete(file)
-    # just check if 'exception' is in the error output to know whether it failed
-    success = "exception" not in error.lower()
-    return {
-        "success": success,
-        "errstring": error,
-        "outstring": output
-    }
+
+    return json.loads(output)
