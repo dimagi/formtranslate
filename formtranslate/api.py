46ad8d6e4fdc8ff0a51288c8004b2f54d7e65195
yedi <yedispaghetti@gmail.com>
2013-06-17 11:13:24 -0400

    refactor so that new functionality is still compatible with old features like the formtranslate UI; fix tests

--- formtranslate/api.py
+++ formtranslate/api.py
@@ -6,21 +6,27 @@ from tempfile import NamedTemporaryFile
 import os
 import json
 
-def validate(input_data, version='1.0'):
+def validate(input_data, version='1.0', output_only=True):
     """Validates an xform into an xsd file"""
-    # hack
-    return form_translate(input_data, "validate", version=version)
+    if version == '1.0':
+        # hack
+        vals = form_translate(input_data, "schema", version=version)
+        vals["outstring"] = ""
+    else:
+        vals = form_translate(input_data, "validate", version=version)
+        if output_only:
+            json.loads(vals["outstring"]) if vals.get("outstring") else {}
+    return vals
 
-def get_xsd_schema(input_data, version='1.0'):
+def get_xsd_schema(input_data, version='1.0', output_only=False):
     """Translates an xform into an xsd file"""
     return form_translate(input_data, "schema", version=version)
 
-def readable_form(input_data, version='1.0'):
+def readable_form(input_data, version='1.0', output_only=False):
     """Gets a readable display of an xform"""
     return form_translate(input_data, "summary", version=version)
 
-
-def csv_dump(input_data, version='1.0'):
+def csv_dump(input_data, version='1.0', output_only=False):
     """Get the csv translation file from an xform"""
     return form_translate(input_data, "csvdump", version=version)
 
@@ -38,37 +44,45 @@ def delete(file):
         pass
 
 def form_translate(input_data, operation, version='1.0'):
-    """Utility for interacting with the form_translate jar, which provides 
-       functionality for a number of different useful form tools including 
+    """Utility for interacting with the form_translate jar, which provides
+       functionality for a number of different useful form tools including
        converting a form to an xsd file, turning a form into a more readable
        format, and generating a list of translations as an exportable .csv
        file."""
-    
+
     # In case you're trying to produce this behavior on the command line for
-    # rapid testing, the command that eventually gets called is: 
+    # rapid testing, the command that eventually gets called is:
     # java -jar form_translate.jar <operation> < form.xml > output
     #
     # You can pass in a filename or a full string/stream of xml data
 
-    stdout_w = stdin_w = None
+    stdout_w = stderr_w = stdin_w = None
     location = config.get_form_translate_jar_location(version)
 
     try:
         with open_w() as stdin_w:
             stdin_w.write(input_data)
 
-        with nested(open_w(), open_r(stdin_w)) as (stdout_w, stdin_r):
+        with nested(open_w(), open_w(), open_r(stdin_w)) as (stdout_w, stderr_w, stdin_r):
             Subprocess(
                 ["java","-jar", location, operation],
                 shell=False,
                 stdin=stdin_r,
                 stdout=stdout_w,
+                stderr=stderr_w
             ).run(timeout=getattr(settings, 'FORMTRANSLATE_TIMEOUT', 5))
 
         with open_r(stdout_w) as stdout_r:
             output = stdout_r.read()
+        with open_r(stderr_w) as stderr_r:
+            error = stderr_r.read()
     finally:
-        for file in (stdout_w, stdin_w):
+        for file in (stdout_w, stderr_w, stdin_w):
             delete(file)
-
-    return json.loads(output)
+    # just check if 'exception' is in the error output to know whether it failed
+    success = "exception" not in error.lower()
+    return {
+        "success": success,
+        "errstring": error,
+        "outstring": output
+    }
--- formtranslate/views.py
+++ formtranslate/views.py
@@ -23,7 +23,7 @@ def _wrapped_api_call(request, api_func):
         ret.update(**{"success": False, "errstring": "No form present!"})
     else:
         file = file.encode('utf-8')
-        ret.update(**api_func(file, version))
+        ret.update(**api_func(file, version, output_only=True))
     return HttpResponse(json.dumps(ret), mimetype="text/json")
     
 @require_POST
